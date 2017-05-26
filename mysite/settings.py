b62cc7b7abd983d769ed06ccadd2d579cd298124
Ludovic LEGENDART <ludovic.legendart@gmail.com>
2017-05-26 09:43:54 +0200

    deleting an unlucky print statement

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Custom settings : we add a new directory named "apps" where all
 # our custom code lives. The following line adds "apps" to the pythonpath
 APPS_DIR = os.path.join(BASE_DIR, 'apps')
-print(APPS_DIR)
 sys.path.append(APPS_DIR)
 
 # Quick-start development settings - unsuitable for production
